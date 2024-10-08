a7b6d6f0ae193031c40fc9b9a74477ceee6f9903
李先生 <102724573+3254954760@users.noreply.github.com>
2024-10-08 09:49:34 +0800

    实现lodash的get函数

--- lodash_get/index.js
+++ lodash_get/index.js
@@ -12,12 +12,12 @@ let obj = {
 function get(object,path,defaultValue){
     let arr = path.split(".")
     let result  = object
-    arr.forEach(key=>{
+    for(let key of arr){
         if(result ===null || !(key in result)){
             return defaultValue
         }
         result = result[key]
-    })
+    }
     return result
 }
-get(obj,'a.b.c.d.x',-1)
+console.log(get(obj,'a.b.c.b.x',-1))
