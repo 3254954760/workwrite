f9d9868a6d63ca44c2f58f782f674c8631fe74f5
李先生 <102724573+3254954760@users.noreply.github.com>
2024-10-08 14:45:02 +0800

    浏览器事件循环

---
+++ 宏微任务/index.js
@@ -0,0 +1,47 @@
+setTimeout(() => console.log(1));
+new Promise((resolve) => {
+  resolve();
+
+  console.log(2);
+}).then(() => {
+  setTimeout(() => console.log(3));
+
+  console.log(4);
+
+  Promise.resolve()
+    .then(() => {
+      console.log(5);
+    })
+    .then(() => {
+      Promise.resolve().then(() => {
+        console.log(6);
+      });
+    });
+});
+
+console.log(7);
+
+console.log(1);
+const asyncFunc = async () => {
+    console.log(2);
+    const res = await Promise.resolve();
+    console.log(5);
+    return res;
+};
+asyncFunc().then((res) => {
+    console.log(6);
+    setTimeout(() => {
+        console.log(8);
+    }, 0);
+});
+setTimeout(() => {
+    console.log(9);
+}, 0);
+
+new Promise((resolve) => {
+    console.log(10);
+    resolve();
+}).then(() => {
+    console.log(11);
+})
+// 1 2  10 5  11 6  9  8
