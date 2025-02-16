04fac4b196c3a937c74f972bdf091f74824d0a06
LY325495 <3254954760@qq.com>
2025-02-16 16:12:34 +0800

    SSE流失输出

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
 /**
- * @param {string} s
- * @return {string[][]}
+ * @param {number[][]} matrix
+ * @return {number[]}
  */
-let partition = function (s) {
-    let n = s.length;
+let spiralOrder = function (matrix) {
+    let n = matrix.length;
+    let m = matrix[0].length;
+    let dir = [[0, 1], [1, 0], [0, -1], [-1, 0]];
     let ans = [];
-    let path = [];
-    let isPardorm = (i, j) => {
-        while (i < j) {
-            if (s[i++] !== s[j--]) {
-                return false;
-            }
+    let i = 0;
+    let j = 0;
+    let dirCount = 0;
+    for (let k = 0; k < m * n; k++) {
+        ans[k] = matrix[i][j];
+        matrix[i][j] = Infinity;
+        let x = i + dir[dirCount][0];
+        let y = j + dir[dirCount][1];
+        if (x >= n || x < 0 || y >= m || y < 0 || matrix[x][y] === Infinity) {
+            dirCount = (dirCount + 1) % 4;
         }
-        return true;
-    };
-    let dfs = (i) => {
-        if (i === n) {
-            ans.push(path.slice());
-        }
-        for (let j = i; j < n; j++) {
-            if (isPardorm(i, j)) {
-                path.push(s.substring(i, j + 1));
-                dfs(j + 1);
-                path.pop();
-            }
-        }
-    };
-    dfs(0);
+        i += dir[dirCount][0];
+        j += dir[dirCount][1];
+    }
+
     return ans;
 };
-console.log(partition('aab'));
+console.log(spiralOrder([[1,2,3],[4,5,6],[7,8,9]]));
