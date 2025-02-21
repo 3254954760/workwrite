197e40251b747ca1714b090cb0dbd232e7b119ec
LY325495 <3254954760@qq.com>
2025-02-21 15:32:22 +0800

    数组转化树代码规范

--- 数组和树转化/index.js
+++ 数组和树转化/index.js
@@ -1,70 +1,70 @@
 const arr = [
-    { id: 1, name: 'i1' },
-    { id: 2, name: 'i2', parentId: 1 },
-    { id: 4, name: 'i4', parentId: 3 },
-    { id: 3, name: 'i3', parentId: 2 },
-    { id: 7, name: 'i7', parentId: 3 },
-    { id: 8, name: 'i8', parentId: 3 }
-]
+    {id: 1, name: 'i1'},
+    {id: 2, name: 'i2', parentId: 1},
+    {id: 4, name: 'i4', parentId: 3},
+    {id: 3, name: 'i3', parentId: 2},
+    {id: 7, name: 'i7', parentId: 3},
+    {id: 8, name: 'i8', parentId: 3}
+];
 let arrToTree = (arr) => {
-    let nodeMap ={}
-    let roots =[]
+    let nodeMap = {};
+    let roots = [];
     arr.forEach((item) => {
         nodeMap[item.id] = {
-        ...item,
-        children:[]
-       }
-    })
+            ...item,
+            children: []
+        };
+    });
     arr.forEach(item => {
-        const {id,parentId} =item;
-        if(parentId){
-            nodeMap[parentId].children.push(nodeMap[id])
+        const {id, parentId} = item;
+        if (parentId) {
+            nodeMap[parentId].children.push(nodeMap[id]);
         }
         else {
-            roots.push(nodeMap[id])
+            roots.push(nodeMap[id]);
         }
-    })
-    return roots
-}
-console.log(arrToTree(arr))
+    });
+    return roots;
+};
+console.log(arrToTree(arr));
 
 
 const tree = [
     {
-      id: 1,
-      name: 'i1',
-      children: [
-        {
-          id: 2,
-          name: 'i2',
-          children: [
+        id: 1,
+        name: 'i1',
+        children: [
             {
-              id: 3,
-              name: 'i3',
-              children: []
-            },
-            {
-              id: 4,
-              name: 'i4',
-              children: []
+                id: 2,
+                name: 'i2',
+                children: [
+                    {
+                        id: 3,
+                        name: 'i3',
+                        children: []
+                    },
+                    {
+                        id: 4,
+                        name: 'i4',
+                        children: []
+                    }
+                ]
             }
-          ]
-        }
-      ]
+        ]
     },
     {
-      id: 5,
-      name: 'i5',
-      children: []
+        id: 5,
+        name: 'i5',
+        children: []
     }
 ];
-let treeToArrAns =[]
-let treeToArr = (tree,parentId) => {
+let treeToArrAns = [];
+let treeToArr = (tree, parentId) => {
     tree.forEach(item => {
-        const {id,name,children} = item;
-        treeToArrAns.push({id,name,parentId})
-        treeToArr(children,id)
-    })
-}
-treeToArr(tree,null)
-console.log(treeToArrAns)
+        const {id, name, children} = item;
+        treeToArrAns.push({id, name, parentId});
+        treeToArr(children, id);
+    });
+};
+treeToArr(tree, null);
+console.log(treeToArrAns);
