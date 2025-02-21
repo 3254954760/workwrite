5bd20b68a761dffee365e7729a6b3ca458d6f650
LY325495 <3254954760@qq.com>
2025-02-21 12:43:29 +0800

    格式优化

--- symbol/symbol.js
+++ symbol/symbol.js
@@ -1,24 +1,25 @@
-var symbolStore = {};
-  
-function handleSymbol(description) {
-  if (this instanceof handleSymbol) { // 或者 new.target !==undefined
-    throw new TypeError('Symbol is not a constructor');
-  }
-  var mySymbol = Object.create(null);
-  Object.defineProperty(mySymbol, 'description', {
-    value: description,
-    writable: false,
-    configurable: false
-  });
+let symbolStore = {};
 
-  return mySymbol;
+function handleSymbol(description) {
+    if (this instanceof handleSymbol) { // 或者 new.target !==undefined
+        throw new TypeError('Symbol is not a constructor');
+    }
+    let mySymbol = Object.create(null);
+    Object.defineProperty(mySymbol, 'description', {
+        value: description,
+        writable: false,
+        configurable: false
+    });
+
+    return mySymbol;
 }
 
 handleSymbol.for = function (key) {
-  if (!symbolStore[key]) {
-    symbolStore[key] = handleSymbol(key);
-  }
+    if (!symbolStore[key]) {
+        symbolStore[key] = handleSymbol(key);
+    }
 
-  return symbolStore[key];
-}
-console.log(handleSymbol('sa'))
+    return symbolStore[key];
+};
+
+console.log(handleSymbol('sa'));
