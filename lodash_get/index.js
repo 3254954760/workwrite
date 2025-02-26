fdcbdee76d463f7f0942a049daa23ccac193e3af
有志青年 <3254954760@qq.com>
2025-02-26 20:46:44 +0800

    lodash_get

--- lodash_get/index.js
+++ lodash_get/index.js
@@ -1,84 +1,59 @@
 let obj = {
-    a:{
-        b:{
-            c:{
-                d:{
-                    x:1
+    a: {
+        b: {
+            c: {
+                d: {
+                    x: 1
                 }
             }
         }
     }
-}
-function get(object,path,defaultValue){
-    let arr = path.split(".")
-    let result  = object
-    for(let key of arr){
-        if(result ===null || !(key in result)){
-            return defaultValue
+};
+function get(object, path, defaultValue) {
+    let arr = path.split('.');
+    let result  = object;
+    for (let key of arr) {
+        if (result === null || !(key in result)) {
+            return defaultValue;
         }
-        result = result[key]
+        result = result[key];
     }
-    return result
+    return result;
 }
-console.log(get(obj,'a.b.c.b.x',-1))
-
-let obj = {
-    a:{
-        b:{
-            c:{
-                d:{
-                    x:1
-                }
-            }
-        }
-    }
-}
-function get(object,path,defaultValue){
-    let arr = path.split(".")
-    let result  = object
-    arr.forEach(key=>{
-        if(result ===null || !(key in result)){
-            return defaultValue
-        }
-        result = result[key]
-    })
-    return result
-}
-get(obj,'a.b.c.d.x',-1)
-
+console.log(get(obj, 'a.b.c.b.x', -1));
 
 
 const obj2 = {
     a: {
-      b: {
-        c: 1,
-        d: 2
-      },
-      c: 3
+        b: {
+            c: 1,
+            d: 2
+        },
+        c: 3
     },
     e: 4
-}
+};
 
-let get = (obj) => {
+let deepGet = (obj) => {
     let ans = {};
     let dfs = (obj, level, s) => {
-        if(obj instanceof Object) {
-            Object.keys(obj).forEach( key => {
-                dfs(obj[key], level+1, s+'.' + key);
-            })
+        if (obj instanceof Object) {
+            Object.keys(obj).forEach(key => {
+                dfs(obj[key], level + 1, s + '.' + key);
+            });
         }
-        else{
-           ans[s] = {
-            value: obj,
-            level:level
-           }
+        else {
+            ans[s] = {
+                value: obj,
+                level: level
+            };
         }
     };
 
-    Object.keys(obj).forEach( key => {
-        dfs(obj[key],1,key);
-    })
+    Object.keys(obj).forEach(key => {
+        dfs(obj[key], 1, key);
+    });
 
-    return ans
-}
-、
+    return ans;
+};
+console.log(deepGet(obj2));
