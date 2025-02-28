1809c008834fae66727f1d575870b795f0b95d15
有志青年 <3254954760@qq.com>
2025-02-28 13:29:34 +0800

    最大控制并发请求增加finaly处理

--- 控制最大并发请求/index.js
+++ 控制最大并发请求/index.js
@@ -1,55 +1,54 @@
 
 class Scheduler {
-    constructor(){
-        this.maxTask =2
-        this.queue =[]
-        this.running =0
-        this.map= new Map()
+    constructor() {
+        this.maxTask = 2;
+        this.queue = [];
+        this.running = 0;
+        this.map = new Map();
     }
-  add(task) {
-      this.queue.push(task)
-      return new Promise((resolve,reject)=>{
-          this.map.set(task,{resolve,reject})
-          this.run()
-      })
-   }
-    run(){
-        if(this.running<this.maxTask && this.queue.length>0){
+    add(task) {
+        this.queue.push(task);
+        return new Promise((resolve, reject) => {
+            this.map.set(task, {resolve, reject});
+            this.run();
+        });
+    }
+    run() {
+        if (this.running < this.maxTask && this.queue.length > 0) {
             this.running++;
-            let task = this.queue.shift()
-            const {resolve,reject} = this.map.get(task)
-            task().then((data)=>{
-                this.running--
-                resolve(data)
-                this.run()
-            }).catch(err =>{
-                reject(err)
-                this.running--
-                this.run()
-            })
+            let task = this.queue.shift();
+            const {resolve, reject} = this.map.get(task);
+            task().then((data) => {
+                resolve(data);
+            }).catch(err => {
+                reject(err);
+            }).finaly(() => {
+                this.running--;
+                this.run();
+            });
         }
     }
 }
 
-const timeout = (time) => new Promise((resolve,reject) => {
-  setTimeout(()=>{
-    resolve()
-    reject('报错'+time)
-  }, time)
-})
+const timeout = (time) => new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve();
+        reject('报错' + time);
+    }, time);
+});
 
-const scheduler = new Scheduler()
+const scheduler = new Scheduler();
 const addTask = (time, order) => {
-  scheduler.add(() => timeout(time))
-    .then(() => console.log(order))
-    .catch(err => console.log(err))
-}
+    scheduler.add(() => timeout(time))
+        .then(() => console.log(order))
+        .catch(err => console.log(err));
+};
 
-addTask(1000, '1')
-addTask(500, '2')
-addTask(300, '3')
-addTask(400, '4')
-addTask(600, '6')
+addTask(1000, '1');
+addTask(500, '2');
+addTask(300, '3');
+addTask(400, '4');
+addTask(600, '6');
 
 
 class ConcurrentExecutor {
@@ -58,20 +57,20 @@ class ConcurrentExecutor {
         this.queue = [];
         this.running = 0;
     }
-    
+
     addTask(task) {
         this.queue.push(task);
     }
 
     run() {
-        if(this.queue.length > 0 && this.running <= this.maxConcurrent){
+        if (this.queue.length > 0 && this.running <= this.maxConcurrent) {
             let task =  this.queue.shift();
             this.running++;
             task().then(res => {
                 this.running--;
-                this.run()
-            })
-            this.run()
+                this.run();
+            });
+            this.run();
         }
         return;
     }
