f5c3b60e8d880c5293bef9377727d7dcbe4ad037
有志青年 <3254954760@qq.com>
2025-03-17 13:42:41 +0800

    深拷贝

--- 深浅拷贝/index.js
+++ 深浅拷贝/index.js
@@ -1,25 +1,36 @@
-let obj  ={
-    a:1,
-    b:2,
-    c:3
-}
-let p = structuredClone(obj)
-let q = JSON.parse(JSON.stringify(obj))
+let obj  = {
+    a: 1,
+    b: 2,
+    c: 3
+};
+let p = structuredClone(obj);
+let q = JSON.parse(JSON.stringify(obj));
 function deepClone(target) {
-    if (
-            Object.prototype.toString.call(target) !== '[object Object]'
-        &&  Object.prototype.toString.call(target) !== '[object Array]'
-    ) {
-        return target
+    // 处理特殊对象类型
+    if (target instanceof Date) {
+        copy = new Date(target.getTime());
+        return copy;
     }
-    let copy = Array.isArray(target)? [] : {}
+
+    if (target instanceof RegExp) {
+        copy = new RegExp(target);
+        return copy;
+    }
+
+    if (
+        Object.prototype.toString.call(target) !== '[object Object]'
+        && Object.prototype.toString.call(target) !== '[object Array]'
+    ) {
+        return target;
+    }
+    let copy = Array.isArray(target) ? [] : {};
     Object.keys(target).forEach((key) => {
-        copy[key] = target[key] instanceof Object ? deepClone(target[key]) : target[key]
-    })
-    return copy
+        copy[key] = target[key] instanceof Object ? deepClone(target[key]) : target[key];
+    });
+    return copy;
 };
 
-function shallowCopy(){
+function shallowCopy() {
     let newobj = {};
     for (let key in obj) {
         if (obj.hasOwnProperty(key)) {
